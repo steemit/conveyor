6f9a3b825fa9957d4745c4ae94a569f916f118bd
Benjamin Chodoroff <bc@thermitic.net>
2018-11-07 14:16:52 -0500

    return summarized url even if blacklisted

--- src/summarizer.ts
+++ src/summarizer.ts
@@ -13,6 +13,7 @@ import { JsonRpcAuthMethodContext as JCtx, JsonRpcError } from '@steemit/koa-jso
 import { badDomains, badUrls } from './blacklist'
 
 interface SummarizedUrl {
+    blacklisted?: boolean
     description?: string
     favicon?: string
     image?: string
@@ -27,6 +28,7 @@ const cache = lruCache({
 
 export async function summarizeUrl(this: JCtx, urlStr: string): Promise<SummarizedUrl> {
     let parsedUrl: any
+    let blacklisted: boolean = false
 
     try {
         parsedUrl = url.parse(urlStr)
@@ -36,20 +38,24 @@ export async function summarizeUrl(this: JCtx, urlStr: string): Promise<Summariz
         throw new JsonRpcError(400, 'Cannot parse URL')
     }
 
-    // Refuse to summarize blacklisted domains
-    badDomains.forEach((badDomain) => {
+    // Is domain blacklisted?
+    for (let badDomain of badDomains) {
         if (parsedUrl.host.endsWith(badDomain)) {
-            throw new JsonRpcError(403, 'Domain is blacklisted')
+            blacklisted = true
+            break
         }
-    })
+    }
 
-    // Refuse to summarize blacklisted URLs
-    const urlLessProtocol = `${parsedUrl.host}${parsedUrl.pathname}`
-    badUrls.forEach((badUrl) => {
-        if (urlLessProtocol.startsWith(badUrl)) {
-            throw new JsonRpcError(403, 'URL is blacklisted')
-        }
-    })
+    // Is url blacklisted?
+    if (!blacklisted) {
+      const urlLessProtocol = `${parsedUrl.host}${parsedUrl.pathname}`
+      for (let badUrl of badUrls) {
+          if (urlLessProtocol.startsWith(badUrl)) {
+              blacklisted = true
+              break
+          }
+      }
+    }
 
     // Serve from cache if possible
     if (cache.has(urlStr)) {
@@ -66,6 +72,7 @@ export async function summarizeUrl(this: JCtx, urlStr: string): Promise<Summariz
         const unfluffed = unfluff(urlData.body)
 
         const res = {
+            blacklisted,
             description: unfluffed.description,
             favicon: unfluffed.favicon,
             image: unfluffed.image,
